93580a50a3ec657df283ab7506b693dbcf89372f
Patryk Ptasinski <patryk@ipepe.pl>
2021-03-07 14:16:23 +0100

    Formatted SerialBT

--- esp32-cruise-control/tests/elmduino-test/elmduino-test.ino
+++ esp32-cruise-control/tests/elmduino-test/elmduino-test.ino
@@ -1,50 +1,34 @@
 #include "BluetoothSerial.h"
 #include "ELMduino.h"
 
-
 BluetoothSerial SerialBT;
+
 #define ELM_PORT   SerialBT
 #define DEBUG_PORT Serial
 
-
 ELM327 myELM327;
-
-
 uint32_t rpm = 0;
 
-
-void setup()
-{
-
+void setup(){
   DEBUG_PORT.begin(115200);
-  //SerialBT.setPin("1234");
   ELM_PORT.begin("ArduHUD", true);
-  
-  if (!ELM_PORT.connect("OBDII"))
-  {
+  if (!ELM_PORT.connect("OBDII")){
     DEBUG_PORT.println("Couldn't connect to OBD scanner - Phase 1");
     while(1);
   }
-
-  if (!myELM327.begin(ELM_PORT, true, 2000))
-  {
+  if (!myELM327.begin(ELM_PORT, true, 2000)){
     Serial.println("Couldn't connect to OBD scanner - Phase 2");
     while (1);
   }
-
   Serial.println("Connected to ELM327");
 }
 
-
-void loop()
-{
+void loop(){
   float tempRPM = myELM327.rpm();
-
-  if (myELM327.status == ELM_SUCCESS)
-  {
+  if (myELM327.status == ELM_SUCCESS){
     rpm = (uint32_t)tempRPM;
     Serial.print("RPM: "); Serial.println(rpm);
-  }
-  else
+  }else{
     myELM327.printError();
+  }
 }
